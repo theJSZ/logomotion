0868c4b97904e3af2014d66974489b3006fe6b5b
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2023-09-26 11:56:58 +0300

    editing globals.py

--- logomotion/src/parser/globals.py
+++ logomotion/src/parser/globals.py
@@ -1,7 +1,7 @@
 # pylint: disable=invalid-name, too-few-public-methods
 """Parsing rules and globals used by the parser"""
 
-from .lexer.lexer import Lexer
+from ..lexer.lexer import Lexer
 from code_generator.code_generator import JavaCodeGenerator
 from utils.logger import Logger
 from entities.symbol_tables import SymbolTables
