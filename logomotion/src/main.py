263eaef7b7815dd2fd62f61e12cb4fe278b71140
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2023-09-26 13:08:26 +0300

    restored main.py

--- logomotion/src/main.py
+++ logomotion/src/main.py
@@ -41,32 +41,31 @@ def main():
         err_msg = f"{CODE_GEN_LANG} is not an implemented" "programming language for code generator"
         raise Exception(err_msg)
 
+    def compile_logo():
+        """Compiles a user given logo file and generates code if there are no errors.
+        Prints lexer & parser results if debug flag (-d, --debug) is on."""
 
-def compile_logo(LOGO_CODE):
-    """Compiles a user given logo file and generates code if there are no errors.
-    Prints lexer & parser results if debug flag (-d, --debug) is on."""
+        logger.debug(LOGO_CODE + "\n")
+        # Tokenize
+        tokens = lexer.tokenize_input(LOGO_CODE)
+        logger.debug("Lexer tokens:")
+        logger.debug("\n".join((str(token) for token in tokens)) + "\n")
 
-    logger.debug(LOGO_CODE + "\n")
-    # Tokenize
-    tokens = lexer.tokenize_input(LOGO_CODE)
-    logger.debug("Lexer tokens:")
-    logger.debug("\n".join((str(token) for token in tokens)) + "\n")
+        # Parse and type analyzation
+        start_node = parser.parse(LOGO_CODE)
+        if start_node:
+            start_node.check_types()
+            logger.debug("Parser AST:")
+            logger.debug(console_io.get_formatted_ast(start_node))
 
-    # Parse and type analyzation
-    start_node = parser.parse(LOGO_CODE)
-    if start_node:
-        start_node.check_types()
-        logger.debug("Parser AST:")
-        logger.debug(console_io.get_formatted_ast(start_node))
-
-    # Code generation, if there are no errors
-    if start_node and not error_handler.errors:
-        logger.debug("Generated code:")
-        start_node.generate_code()
-        code_generator.write()
-    else:
-        error_handler.create_json_file()
-        error_handler.write_errors_to_console()
+        # Code generation, if there are no errors
+        if start_node and not error_handler.errors:
+            logger.debug("Generated code:")
+            start_node.generate_code()
+            code_generator.write()
+        else:
+            error_handler.create_json_file()
+            error_handler.write_errors_to_console()
 
     # Create required classes for the compiler
     console_io = ConsoleIO()
@@ -84,15 +83,15 @@ def compile_logo(LOGO_CODE):
     compile_logo()
 
 
-# if __name__ == "__main__":
-def logotesti(filename):
-    # def get_cmd_line_args():
-    #     arg_parser = argparse.ArgumentParser(
-    #         prog="Logomotion", description="Compile logo to java via python"
-    #     )
-    #     arg_parser.add_argument("filepath")
-    #     arg_parser.add_argument("-d", "--debug", action="store_true")
-    #     return arg_parser.parse_args()
+if __name__ == "__main__":
+
+    def get_cmd_line_args():
+        arg_parser = argparse.ArgumentParser(
+            prog="Logomotion", description="Compile logo to java via python"
+        )
+        arg_parser.add_argument("filepath")
+        arg_parser.add_argument("-d", "--debug", action="store_true")
+        return arg_parser.parse_args()
 
     def load_file(filename):
         """Loads a file and returns contents as a string."""
@@ -107,10 +106,10 @@ def logotesti(filename):
     CODE_GEN_LANG = os.getenv("CODE_GEN_LANG")
 
     # Get command line arguments
-    # args = get_cmd_line_args()
+    args = get_cmd_line_args()
 
     # Get logo code from file. Filepath is given as a command line argument
-    # LOGO_CODE = load_file(args.filepath)
+    LOGO_CODE = load_file(args.filepath)
     LOGO_CODE = load_file(filename)
 
     main()
